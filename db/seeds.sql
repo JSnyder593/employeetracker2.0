22a7ad3dda80756a334f6a04f5567d074f98955e
JSnyder593 <jaeger_snyder@hotmail.com>
2022-12-16 20:46:35 -0800

    created schema and seeds for db

---
+++ db/seeds.sql
@@ -0,0 +1,19 @@
+INSERT INTO departments (name)
+VALUES  ("Overwatch"),
+        ("Talon");
+
+INSERT INTO roles (title, department, salary)
+VALUES  ("Tank", 1 or 2, 125000),
+        ("Damage", 1 or 2, 100000),
+        ("Support", 1 or 2, 80000),
+        ("Tank", 1 or 2, 150000),
+        ("Damage", 1 or 2, 125000),
+        ("Support", 1 or 2, 100000);
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES  ("Ana", "Amari", 3, 0),
+        ("Moira", "O'Deorain", 6, 0),
+        ("Gabriel", "Reyes", 5, 0),
+        ("Jack", "Morrison", 2, 0),
+        ("Akande, 'Doomfist'", "Ogundimu", 4, 0),
+        ("Reinhardt", "Wilhelm", 1, 0);
